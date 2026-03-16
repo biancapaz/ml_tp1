c4b9855b7c70bd0e9ec30b14a3b540c9dc3082fa
Bianca Paz <bpaz@udesa.edu.ar>
2026-03-15 22:51:32 -0300

    agregue cosas de EDA y edite models

--- src/models.py
+++ src/models.py
@@ -1,3 +1,18 @@
 # Clases para los modelos de ML
 
-#class LinearRegression()
+class LinearRegression():
+    def __init__(self):
+        self.coef_inv
+        self.coef_grad
+
+    def training1(self):
+        #entrenamiento por pseudo-inversa
+        pass
+
+    def treaining2(self):
+        #entrnamiento por descenso de gradiente
+        pass
+
+    def show_coef(self):
+        #imprime coefs con sus variables
+        pass
